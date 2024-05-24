40e90baf763712aff6e44ee78e6bce52ec5ce83b
Aeyu17 <jami3507@gmail.com>
2024-05-23 20:31:00 -0500

    Added teensy crash handling as implemented on arm

--- core_ws/src/core_pkg/core_pkg/core_control_node.py
+++ core_ws/src/core_pkg/core_pkg/core_control_node.py
@@ -2,6 +2,10 @@ import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Empty
 
+import signal
+import time
+import atexit
+
 import serial
 import sys
 import threading
@@ -9,6 +13,9 @@ import glob
 
 from std_msgs.msg import String
 from interfaces_pkg.msg import CoreFeedback
+
+serial_pub = None
+thread = None
 
 class SerialRelay(Node):
     def __init__(self):
@@ -29,37 +36,45 @@ class SerialRelay(Node):
         # Loop through all serial devices on the computer to check for the MCU
         self.port = None
         ports = SerialRelay.list_serial_ports()
-        for port in ports:
-            try:
-                # connect and send a ping command
-                ser = serial.Serial(port, timeout=1)
-                ser.write(b"ping\n")
-                response = ser.read_until("\n")
+        for i in range(2):
+            for port in ports:
+                try:
+                    # connect and send a ping command
+                    ser = serial.Serial(port, timeout=1)
+                    ser.write(b"ping\n")
+                    response = ser.read_until("\n")
 
-                # if pong is in response, then we are talking with the MCU
-                if b"pong" in response:
-                    self.port = port
-                    print(f"Found MCU at {self.port}!")
-                    break
-            except:
-                pass
+                    # if pong is in response, then we are talking with the MCU
+                    if b"pong" in response:
+                        self.port = port
+                        print(f"Found MCU at {self.port}!")
+                        break
+                except:
+                    pass
+            if self.port is not None:
+                break
         
         if self.port is None:
             print("Unable to find MCU... please make sure it is connected.")
+            time.sleep(1)
             sys.exit(1)
         
         self.ser = serial.Serial(self.port, 115200)
+        atexit.register(self.cleanup)
+
 
         #self.heartbeat_client = self.create_client(Empty, '/astra/core/heartbeat')
         #self.connected: bool = False
         #while not self.heartbeat_client.wait_for_service(timeout_sec=2.0):
         #    self.get_logger().info("Could not connect to Base Station. Retrying...")
-
+    
 
         #threading.Thread(target=self.heartbeat_thread, daemon=True)
+        
 
     def run(self):
         # This thread makes all the update processes run in the background
+        global thread
         thread = threading.Thread(target=rclpy.spin, args={self}, daemon=True)
         thread.start()
         
@@ -73,33 +88,51 @@ class SerialRelay(Node):
             sys.exit(0)
 
     def read_MCU(self):
-        output = str(self.ser.readline(), "utf8")
-        
-        if output:
-            packet = output.strip().split(',')
-            if len(packet) >= 2 and packet[0] == "core" and packet[1] == "feedback":
-                feedback = CoreFeedback()
-                feedback.gpslat = float(packet[2])
-                feedback.gpslon = float(packet[3])
-                feedback.gpssats = float(packet[4])
-                feedback.bnogyr.x = float(packet[5])
-                feedback.bnogyr.y = float(packet[6])
-                feedback.bnogyr.z = float(packet[7])
-                feedback.bnoacc.x = float(packet[8])
-                feedback.bnoacc.y = float(packet[9])
-                feedback.bnoacc.z = float(packet[10])
-                feedback.orient = float(packet[11])
-                feedback.bmptemp = float(packet[12])
-                feedback.bmppres = float(packet[13])
-                feedback.bmpalt = float(packet[14])
+        try:
+            output = str(self.ser.readline(), "utf8")
+            
+            if output:
+                packet = output.strip().split(',')
+                if len(packet) >= 2 and packet[0] == "core" and packet[1] == "feedback":
+                    feedback = CoreFeedback()
+                    feedback.gpslat = float(packet[2])
+                    feedback.gpslon = float(packet[3])
+                    feedback.gpssats = float(packet[4])
+                    feedback.bnogyr.x = float(packet[5])
+                    feedback.bnogyr.y = float(packet[6])
+                    feedback.bnogyr.z = float(packet[7])
+                    feedback.bnoacc.x = float(packet[8])
+                    feedback.bnoacc.y = float(packet[9])
+                    feedback.bnoacc.z = float(packet[10])
+                    feedback.orient = float(packet[11])
+                    feedback.bmptemp = float(packet[12])
+                    feedback.bmppres = float(packet[13])
+                    feedback.bmpalt = float(packet[14])
 
-                self.telemetry_publisher.publish(feedback)
-            else:
-                print(f"[MCU] {output}", end="")
-                msg = String()
-                msg.data = output
-                self.feedback_publisher.publish(msg)
-                return
+                    self.telemetry_publisher.publish(feedback)
+                else:
+                    print(f"[MCU] {output}", end="")
+                    msg = String()
+                    msg.data = output
+                    self.feedback_publisher.publish(msg)
+                    return
+        except serial.SerialException:
+            print("SerialException caught... closing serial port.")
+            if self.ser.is_open:
+                self.ser.close()
+            pass
+        except TypeError as e:
+            print(f"TypeError: {e}")
+            print("Closing serial port.")
+            if self.ser.is_open:
+                self.ser.close()
+            pass
+        except Exception as e:
+            print(f"Exception: {e}")
+            print("Closing serial port.")
+            if self.ser.is_open:
+                self.ser.close()
+            pass
                 
 
 
@@ -128,15 +161,30 @@ class SerialRelay(Node):
     @staticmethod
     def list_serial_ports():
         return glob.glob("/dev/ttyUSB*") + glob.glob("/dev/ttyACM*")
+    
+    def cleanup(self):
+        print("Cleaning up...")
+        if self.ser.is_open:
+            self.ser.close()
+
+def myexcepthook(type, value, tb):
+    print("Uncaught exception:", type, value)
+    if serial_pub:
+        serial_pub.cleanup()
       
 
 
 def main(args=None):
    rclpy.init(args=args)
+   sys.excepthook = myexcepthook
+
+   global serial_pub
 
    serial_pub = SerialRelay()
    serial_pub.run()
 
 if __name__ == '__main__':
-   main()
+    signal.signal(signal.SIGTSTP, lambda signum, frame: sys.exit(0))  # Catch Ctrl+Z and exit cleanly
+    signal.signal(signal.SIGTERM, lambda signum, frame: sys.exit(0))  # Catch termination signals and exit cleanly
+    main()
 
