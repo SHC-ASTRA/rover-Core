87fee274b98fa5601b18a4d2b7bef709474a70c0
Tristan McGinnis <tristanmcginnis26@gmail.com>
2025-02-13 12:15:53 -0600

    Updates core_control for new interface & vicCAN

--- core_ws/src/core_pkg/core_pkg/core_control_node.py
+++ core_ws/src/core_pkg/core_pkg/core_control_node.py
@@ -12,8 +12,8 @@ import threading
 import glob
 
 from std_msgs.msg import String
-from ...ROS2_Interfaces.interfaces_pkg.msg import CoreFeedback
-from ...ROS2_Interfaces.interfaces_pkg.msg import ControllerState
+from ...ROS2_Interfaces.interfaces_pkg.msg.feedback import CoreFeedback
+from ...ROS2_Interfaces.interfaces_pkg.msg.control import CoreControl
 
 serial_pub = None
 thread = None
@@ -23,15 +23,13 @@ class SerialRelay(Node):
         # Initalize node with name
         super().__init__("serial_publisher")#previously 'serial_publisher'
 
-        # Create a publisher to publish any output the MCU sends
-        self.feedback_publisher = self.create_publisher(String, '/astra/core/feedback', 10) 
-        self.telemetry_publisher = self.create_publisher(CoreFeedback, '/astra/core/telemetry', 10)
+        # Create publishers for feedback and telemetry
+        self.debug_pub = self.create_publisher(String, '/core/debug', 10) 
+        self.feedback_pub = self.create_publisher(CoreFeedback, '/core/feedback', 10)
 
         # Create a subscriber to listen to any commands sent for the MCU
-        # Create a subscriber to listen to any commands sent for the MCU
-        self.subscriber = self.create_subscription(ControllerState, '/astra/core/control', self.send_controls, 10)
+        self.control_sub = self.create_subscription(CoreControl, '/core/control', self.send_controls, 10)
         
-
 
         # Create a service server for pinging the rover
         self.ping_service = self.create_service(Empty, '/astra/core/ping', self.ping_callback)
@@ -59,22 +57,13 @@ class SerialRelay(Node):
                 break
         
         if self.port is None:
-            print("Unable to find MCU... please make sure it is connected.")
+            print("Unable to find MCU...")
             time.sleep(1)
             sys.exit(1)
         
         self.ser = serial.Serial(self.port, 115200)
         atexit.register(self.cleanup)
 
-
-        #self.heartbeat_client = self.create_client(Empty, '/astra/core/heartbeat')
-        #self.connected: bool = False
-        #while not self.heartbeat_client.wait_for_service(timeout_sec=2.0):
-        #    self.get_logger().info("Could not connect to Base Station. Retrying...")
-    
-
-        #threading.Thread(target=self.heartbeat_thread, daemon=True)
-        
 
     def run(self):
         # This thread makes all the update processes run in the background
@@ -84,10 +73,7 @@ class SerialRelay(Node):
         
         try:
             while rclpy.ok():
-                # Check the MCU for updates
-                self.read_MCU()
-                #if not self.connected:
-                #    self.send("auto,stop")
+                self.read_MCU() # Check the MCU for updates
         except KeyboardInterrupt:
             sys.exit(0)
 
@@ -96,33 +82,42 @@ class SerialRelay(Node):
             output = str(self.ser.readline(), "utf8")
             
             if output:
-                packet = output.strip().split(',')
-                
-                if len(packet) >= 2 and packet[0] == "core" and packet[1] == "telemetry":
-                    feedback = CoreFeedback()
-                    feedback.gpslat = float(packet[2])
-                    feedback.gpslon = float(packet[3])
-                    feedback.gpssat = float(packet[4])
-                    feedback.bnogyr.x = float(packet[5])
-                    feedback.bnogyr.y = float(packet[6])
-                    feedback.bnogyr.z = float(packet[7])
-                    feedback.bnoacc.x = float(packet[8])
-                    feedback.bnoacc.y = float(packet[9])
-                    feedback.bnoacc.z = float(packet[10])
-                    feedback.orient = float(packet[11])
-                    feedback.bmptemp = float(packet[12])
-                    feedback.bmppres = float(packet[13])
-                    feedback.bmpalt = float(packet[14])
+                # All output over debug temporarily
+                print(f"[MCU] {output}", end="")
+                msg = String()
+                msg.data = output
+                self.debug_pub.publish(msg)
+                return
+                # Temporary
 
-                    self.telemetry_publisher.publish(feedback)
-                else:
-                    print(f"[MCU] {output}", end="")
-                    msg = String()
-                    msg.data = output
-                    self.feedback_publisher.publish(msg)
-                    return
-        except serial.SerialException:
-            print("SerialException caught... closing serial port.")
+                # packet = output.strip().split(',')
+                
+                # if len(packet) >= 2 and packet[0] == "core" and packet[1] == "telemetry":
+                #     feedback = CoreFeedback()
+                #     feedback.gpslat = float(packet[2])
+                #     feedback.gpslon = float(packet[3])
+                #     feedback.gpssat = float(packet[4])
+                #     feedback.bnogyr.x = float(packet[5])
+                #     feedback.bnogyr.y = float(packet[6])
+                #     feedback.bnogyr.z = float(packet[7])
+                #     feedback.bnoacc.x = float(packet[8])
+                #     feedback.bnoacc.y = float(packet[9])
+                #     feedback.bnoacc.z = float(packet[10])
+                #     feedback.orient = float(packet[11])
+                #     feedback.bmptemp = float(packet[12])
+                #     feedback.bmppres = float(packet[13])
+                #     feedback.bmpalt = float(packet[14])
+
+                #     self.telemetry_publisher.publish(feedback)
+                # else:
+                #     # print(f"[MCU] {output}", end="")
+                #     # msg = String()
+                #     # msg.data = output
+                #     # self.debug_pub.publish(msg)
+                #     return
+        except serial.SerialException as e:
+            print(f"SerialException: {e}")
+            print("Closing serial port.")
             if self.ser.is_open:
                 self.ser.close()
             pass
@@ -140,28 +135,17 @@ class SerialRelay(Node):
             pass
                 
 
-
     def send_controls(self, msg):
-        #command = msg.data + '\n'
-        command = "ctrl," + msg.ls_y + ',' + msg.rs_y + '\n'
-        print(f"[Sys] {command}", end="")
-
-        # Send command to MCU
-        self.ser.write(bytes(command, "utf8"))
+        #can_relay_tovic,core,19, left_stick, right_stick 
+        command = "can_relay_tovic,core,19," + msg.left_stick/(msg.max_speed/100.0) + ',' + msg.right_stick/(msg.max_speed/100.0) + '\n'
+        #print(f"[Sys] {command}", end="")
+        
+        self.ser.write(bytes(command, "utf8"))# Send command to MCU
         #print(f"[Sys] Relaying: {command}")
 
 
     def ping_callback(self, request, response):
         return response
-
-    def heartbeat_thread(self):
-        while True:
-            response = self.heartbeat_client.call_async(Empty.Request())
-            rclpy.spin_until_future_complete(self, response, timeout_sec=3.0)
-            if response:
-                self.connected = True
-            else:
-                self.connected = False
 
 
     @staticmethod
@@ -169,7 +153,7 @@ class SerialRelay(Node):
         return glob.glob("/dev/ttyUSB*") + glob.glob("/dev/ttyACM*")
     
     def cleanup(self):
-        print("Cleaning up...")
+        print("Cleaning up before terminating...")
         if self.ser.is_open:
             self.ser.close()
 
